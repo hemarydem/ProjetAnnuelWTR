da5f3bc6b8752bcfa0aedb1a4a3c1c370f4f7b32
Remy Hamed <hamedremy@hotmail.fr>
2020-03-23 18:49:22 +0100

    mise a jour admin 2

--- html/Administration/UserProfile.php
+++ html/Administration/UserProfile.php
@@ -10,6 +10,7 @@ if(isset($_GET['email'])) {
     header('location: searchUser.php');
     exit;
 }
+$_SESSION['']
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,32 +34,43 @@ if(isset($_GET['email'])) {
             <h2 class="profilContainTitle">email </h2><h3 id = 'userEmail' class="data"> <?php echo $results[0]['email'];?></h3>
             <!--<button onclick="emailChange()">Click me</button>-->
             <form method="post" action="usersEmailProcess.php">
-               <?php echo '<input type="text" name="oldEmail" placeholder="vieux mail" value="'.$results[0]['email'].'\" >';?>
+               <?php echo '<input type="text" name="oldEmail" placeholder="vieux mail" value="'.$results[0]['email'].'" >';?>
                 <input type="text" name="newEmail" placeholder="nouveaux mail" >
                 <input type="submit">
             </form>
         </div>
+
         <div class="profilContain" id="divLogin">
-            <h2class="profilContainTitle">login </h2> <h3 id = 'userLogin'><?php echo $results[0]['login'];?></h3>
-            
-            <button onclick=pseudoChange()>changer</button>
+            <h2 class="profilContainTitle">login </h2> <h3 id = 'userLogin'><?php echo $results[0]['login'];?></h3>
+            <!--<button onclick=pseudoChange()>changer</button>-->
             <form method="post" action="usersLoginProcess.php">
-               <?php 
-               echo '<input type="text" name="mail" placeholder="vieux mail" value="'.$results[0]['email'].'\" >';?>
-                <input type="text" name="newLogin" placeholder="nouveaux mail" >
-                <input type="submit">
-            </form>
+               <?php echo '<input type="text" name="mail" placeholder="vieux mail" value="'.$results[0]['email'].'" >';?>
+                <input type="text" name="newLogin" placeholder="nouveaux Pseudo" >
                 <input type="submit">
             </form>
         </div>
+
         <div class="profilContain" id="divModerator">
             <h2 class="profilContainTitle">moderator status </h2> <h3 id = 'moderatorStatus'><?php echo $results[0]['moderator'];?></h3>
-            <button onclick=modeStatChange()>changer</button>
+            <!--<button onclick=modeStatChange()>changer</button>-->
+            <form method="post" action="usersModeratorStatusProcess.php">
+               <?php
+               echo '<input type="text" name="mail" value="'.$results[0]['email'].'" >
+                    <input type="text" name="moderator" value="'.$results[0]['moderator'].'">';?>
+                <input type="submit">
+            </form>
         </div>
+
         <div class="profilContain" id="divActivate">
             <h2 class="profilContainTitle">activate </h2> <h3 id = 'userActiveState'><?php echo $results[0]['working'];?></h3>
             <input type="text" id="" placeholder="nouvelle adresse mail"> 
             <button onclick=turnOnOff()>changer</button>
+            <form method="post" action="usersLoginProcess.php">
+               <?php echo '<input type="text" name="mail" placeholder="vieux mail" value="'.$results[0]['email'].'" >';?>
+                <input type="text" name="newLogin" placeholder="nouveaux Pseudo" >
+                <input type="submit">
+            </form>
+            
         </div>
         <div class="profilContain" id="divLevel">
             <h2 class="profilContainTitle">level </h2> <h3 id = 'userLevel'><?php echo $results[0]['userLevel'];?></h3>
