8152181402294fdbe35e9897258b397bacc58281
Omnes Theo <tomnesmouhamadou@myges.fr>
2020-03-26 17:29:44 +0100

    maj fromulaire button placeholder

--- html/signUp.php
+++ html/signUp.php
@@ -24,38 +24,42 @@
 
 			<form id="formSignUp" name="signUp" method="POST" action="signUpProcess.php" >
 				<div>
-				
-					<h1>Inscription</h1>
+					<div>
 					
+						<h1>Inscription</h1>
+						
+						
+						<h3 id="msg">
+							<?php
+								if(isset($_GET['msg'])){
+									echo htmlspecialchars($_GET['msg']);
+								}
+							?>
+						</h3>
+						<section id="inputsSignUp">
+							<label for="">Pseudo</label>
+							<input type="text" name="pseudo" placeholder="Entre 5 et 16 caractères" autocomplete="on" required <?php echo isset( $_GET['pseudo'] ) ? "value=" . htmlspecialchars($_GET['pseudo']) : "" ?> >
+							<label for="">Email</label>
+							<input type="email" name="email" placeholder=" Email" autocomplete="on" required <?php echo isset( $_GET['email'] ) ? "value=" . htmlspecialchars($_GET['email']) : "" ?>>
+							<label for="">Mot de passe</label>
+							<input type="password" name="password" placeholder="1 majuscule 1 chiffre et 1 caractère spécial" autocomplete="on" required>
+							<label for="">Confirmation du mot de passe</label>
+							<input type="password" name="password2" placeholder=" Confirmez le mot de passe" autocomplete="on" required>
+						</section>
+					</div>
 					
-					<h3 id="msg">
-						<?php
-							if(isset($_GET['msg'])){
-								echo htmlspecialchars($_GET['msg']);
-							}
-						?>
-					</h3>
-					<section id="inputsSignUp">
-						<input type="text" name="pseudo" placeholder=" Pseudo" autocomplete="on" required <?php echo isset( $_GET['pseudo'] ) ? "value=" . htmlspecialchars($_GET['pseudo']) : "" ?> >
-						<small class="rules" id="indicationPseudo">* Le pseudo doit faire entre 5 et 16 caractères</small>
-						<input type="email" name="email" placeholder=" Email" autocomplete="on" required <?php echo isset( $_GET['email'] ) ? "value=" . htmlspecialchars($_GET['email']) : "" ?>>
-						<input type="password" name="password" placeholder=" Mot de passe" autocomplete="on" required>
-						<small class="rules" id = "indicationPwd">* Le mot de passe doit contenir au moins 1 majuscule, 1 chiffre et 1 caractère spécial</small>
-						<input type="password" name="password2" placeholder=" Confirmez le mot de passe" autocomplete="on" required>
-					</section>
+					<div>
+						<section id="captchaZone">
+							<?php include('includes/captcha.php') ?>
+							</div>
+						</section>
+					</div>
 				</div>
-				
-				<div>
-					<section id="captchaZone">
-						<?php include('includes/captcha.php') ?>
-						</div>
-					</section>
 
-					<section class="formBottom">
+				<section class="formBottom">
 						<input class="formButton" type="submit" name="submit" value="S'inscrire"><br>
 						<a href="signIn.php">Déjà un compte ? Se connecter</a>
 					</section>
-				</div>
 				
 				
 
