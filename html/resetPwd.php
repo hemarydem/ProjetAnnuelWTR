76e108f2fd03b4d5388da5e74e3910b92e77e79a
Remy Hamed <hamedremy@hotmail.fr>
2020-03-26 19:36:38 +0100

    maj

---
+++ html/resetPwd.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require('includes/config.php');
+
+if( !isset($_GET['login']) || !isset($_GET['token']) ){
+
+  header('location: error.html');
+  exit;
+
+}else {
+
+  $loginGET = $_GET['login'];
+  $tokenGET = $_GET['token'];
+
+  $q = 'SELECT token FROM USERS WHERE login = ?';
+  $req = $bdd->prepare($q);
+  $req->execute([$loginGET]);
+  $tokenBDD = $req->fetch();
+
+  if(count($tokenBDD) > 0 && $tokenGET ==  $tokenBDD[0]) {
+
+    $q = 'UPDATE USERS SET working = 1 WHERE login = ?';
+    $req = $bdd->prepare($q);
+    $req->execute([$loginGET]);
+    header('location:signIn.php?msg=Votre compte est maintenant actif');
+    exit;
+    echo 'OK';
+
+  }else {
+    header('location: error.html');
+    exit;
+  }
+}
+?>
