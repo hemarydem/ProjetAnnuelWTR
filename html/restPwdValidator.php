bcc12b6bcfb1050e15a5a5449df18471eb0dfb31
Omnes Theo <tomnesmouhamadou@myges.fr>
2020-03-26 23:15:23 +0100

    reset mot de passe fonctionnel

--- html/restPwdValidator.php
+++ html/restPwdValidator.php
@@ -1,11 +1,13 @@
 <?php
      include('includes/config.php');
 
+     $newPassword = $_POST['password'];
+     $newPassword = hash('sha256', $newPassword ); 
+
      echo $_GET['login'];
      $q = 'UPDATE USERS SET pass = ? WHERE login = ? ';
      $req = $bdd->prepare($q);
-     $req->execute([$_POST['password'],$_GET['login']]);
+     $req->execute([$newPassword,$_GET['login']]);
      header('location:signIn.php?msg=Votre compte est maintenant actif');
-     //exit;
-
+     exit;
 ?>
